2481c1b57fb64230acc3055f6f144829bf262482
Jeneil Stallion <jeneilstallion@gmail.com>
2021-02-02 16:54:29 -0500

    updates 2/1/21

--- javascript.js
+++ javascript.js
@@ -1,11 +1,12 @@
-const question1 = document.getElementsByName('question-1');
-for (choice of question1) {
-    // code block to be executed
-    if(choice.checked){
-        alert(choice.value);
-        break;
-    }
-  }
-  question1.addEventListener('change',function () {
-    console.log("test log")
-});
+// Alert for question options
+// const radios = document.querySelectorAll('input[type=radio]');
+// for (let radio of radios) {
+//   radio.addEventListener('change', () => alert(radio.value));
+// }
+let submitButton = document.getElementById('submit-button')
+submitButton.addEventListener('submit', () => window.location.href = "https://google.com")
+// submitButton.addEventListener('click', () => window.location.href = "./resources.html")
+
+import * as data from './NIMH_data.json';
+// const {name} = data;
+console.log(data); // output 'testing'
